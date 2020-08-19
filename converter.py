543dfebc0847a6b61717602a102df09b6119cfd2
Shreya Bhattacharya <64976753+theshreyathing@users.noreply.github.com>
2020-08-19 23:21:05 +0530

    Update converter.py

--- converter.py
+++ converter.py
@@ -6,10 +6,3 @@ while True:
     engine.runAndWait()
     if speech == 'exit':
         break 
-sound = engine.getProperty('voices')
-print(sound)
-engine.setProperty('voice',sound[0].id)
-engine.say("I will speak in Male voice")
-engine.setProperty('voice',sound[1].id)
-engine.say("I will speak this text in female voice")
-engine.runAndWait()
